cd12f7913becbc3561e3cca2c80aaf4021c10387
thenora <thenora@gmail.com>
2020-05-27 13:27:11 -0700

    customers index created - passing tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,10 @@
 class CustomersController < ApplicationController
   def index
+    customer = Customer.all.as_json(only: [:id, :name, :registered_at,
+      :postal_code, :phone, :videos_checked_out_count])
+    render json: customer, status: :ok
   end
 end
+
+
+# name, registered_at, address*, city*, state*, postal_code, phone, videos_checked_out_count [* not included in API response]
