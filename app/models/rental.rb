057713f2528b5465df3a88b46e3a4e74293d6ada
thenora <thenora@gmail.com>
2020-05-29 13:31:22 -0700

    rentals validations & tests

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,6 +2,8 @@ class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :video
 
+  validates :customer_id, :video_id, :due_date, :checkout_date, presence: true
+
   def checkout_update(date)
     self.checkout_date = date
     self.due_date = date + 7
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,68 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+
+let(:rental1) {
+  Rental.create(
+    video_id: videos(:video1).id,
+    customer_id: customers(:customer1).id,
+    due_date: Date.today + 3,
+    checkout_date: Date.today - 3
+  )
+}
+
+  describe "validations" do
+    it 'is valid when all fields are present' do
+      # Act
+      result = rental1.valid?
+
+      # Assert
+      expect(result).must_equal true
+    end
+
+    it 'is invalid without a customer' do
+      # Arrange
+      rental1.customer_id = nil
+    
+      # Act
+      result = rental1.valid?
+    
+      # Assert
+      expect(result).must_equal false
+    end
+
+    it 'is invalid without a video' do
+      # Arrange
+      rental1.video_id = nil
+    
+      # Act
+      result = rental1.valid?
+    
+      # Assert
+      expect(result).must_equal false
+    end
+
+    it 'is invalid without a due date' do
+      # Arrange
+      rental1.due_date = nil
+    
+      # Act
+      result = rental1.valid?
+    
+      # Assert
+      expect(result).must_equal false
+    end
+
+    it 'is invalid without a checkout date' do
+      # Arrange
+      rental1.checkout_date = nil
+    
+      # Act
+      result = rental1.valid?
+    
+      # Assert
+      expect(result).must_equal false
+    end
+  end
 end
